2315e7fe3cd883a2888f566815c4424c0dc5e197
Julie Wu <wu.chaojung@gmail.com>
2016-11-25 13:59:35 -0500

    add a little javadoc for one of the function prototype

--- cours6/interface.h
+++ cours6/interface.h
@@ -1,3 +1,6 @@
+// Types
+// -----
+
 typedef void * Definition;
 typedef struct dictionnaire * Dictionnaire;
 typedef enum statut {OK,
@@ -5,6 +8,11 @@ typedef enum statut {OK,
                      ERR_INSERE_ENTREE_EXISTANTE,
                      ERR_DICTIONNAIRE_VIDE} Statut;
 
+/**
+ * Cree une dictionnaire vide et la retourne.
+ *
+ * @returns  La dictionnaire vide
+ */
 Dictionnaire dictionnaireCreer();
 Dictionnaire dictionnaireInsereEntree
              (struct dictionnaire *, char *, void *);
